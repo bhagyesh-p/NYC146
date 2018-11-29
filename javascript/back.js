b84eefcdb0588c3991d19b07b2563cbf331aec10
dogboy602k <rbpatel51@gmail.com>
2018-11-28 20:40:31 -0500

    added the data storage for specific seasons and prices

--- javascript/back.js
+++ javascript/back.js
@@ -3,30 +3,26 @@
 
 class Post {
 
-  constructor(name, info, imageLocation, webLink, linkCaption, imageCaption) 
-  {
+  constructor(name, info, imageLocation, webLink, linkCaption, imageCaption) {
     this._name = name,
-    this._info = info,
-    this._imageLocation = imageLocation,
-    this._webLink = webLink;
+      this._info = info,
+      this._imageLocation = imageLocation,
+      this._webLink = webLink;
     this._linkCaption = linkCaption;
     this._imageCaption = imageCaption;
   }
 
-  build()
-  {
+  build() {
     var largeListItem = document.createElement("li");
 
     var div = document.createElement("div");
     div.className = 'activity';
 
     var ul = document.createElement("ul");
-
     var smallListItem1 = document.createElement("li");
-
     var h1 = document.createElement("h1");
-
     var a = document.createElement("a");
+
     a.className = 'activity-title';
     a.href = this._webLink;
     a.title = this._linkCaption;
@@ -39,12 +35,21 @@ class Post {
 
     var smallListItem3 = document.createElement("li");
 
+
     var img = document.createElement("img");
     img.className = "activity-image";
     img.src = this._imageLocation;
     img.title = this._imageCaption;
     img.alt = "error";
 
+    console.log(h1);
+    console.log(h2);
+    console.log(a);
+    console.log(smallListItem1);
+    console.log(img);
+    console.log(ul);
+    console.log(div);
+    console.log(largeListItem);
 
     h1.appendChild(a);
     smallListItem1.appendChild(h1);
@@ -65,46 +70,100 @@ class Post {
 
 // Have most if not allthe code within the onload, especially
 //if it has to do with animtions
+
 window.onload = () => {
-    document.body.style.overflow = "hidden";
-    // fade in and out the messages at the beginning
+  document.body.style.overflow = "hidden";
+  // fade in and out the messages at the beginning
+  // only occurs if we have the specific items on the page
+  try {
     setTimeout(function() {
-        fadeItem('#messageFront1', "in");
+      fadeItem('#messageFront1', "in");
     }, 1000);
     setTimeout(function() {
-        fadeItem('#messageFront1', "out");
+      fadeItem('#messageFront1', "out");
     }, 2000);
     setTimeout(function() {
-        fadeItem('#messageFront2', "in");
+      fadeItem('#messageFront2', "in");
     }, 3500);
     setTimeout(function() {
-        fadeItem('#messageFront2', "out");
+      fadeItem('#messageFront2', "out");
     }, 7000);
     setTimeout(function() {
-        fadeItem('#messageFront3', "in");
+      fadeItem('#messageFront3', "in");
     }, 8000);
     setTimeout(function() {
-        fadeItem('#messageFront3', "out");
+      fadeItem('#messageFront3', "out");
     }, 11000);
     setTimeout(function() {
-        fadeItem('#frontPage', "out");
+      fadeItem('#frontPage', "out");
     }, 12000);
+  } catch (e) {
+
+  } finally {
+
+  }
 
 
-    function fadeItem(s, action) {
-        if (action === "in") {
-            $(s).fadeIn("slow", function() {});
-            console.log(s + " in");
-        } else if (action === "out") {
-            $(s).fadeOut("slow", function() {});
-            console.log(s + " out");
-        } else {
-            alert("error with fadeing of item: " + s)
-        }
+
+  function fadeItem(s, action) {
+    if (action === "in") {
+      $(s).fadeIn("slow", function() {});
+    } else if (action === "out") {
+      $(s).fadeOut("slow", function() {});
+    } else {
+      alert("error with fadeing of item: " + s)
     }
+  }
 
-    let place = new Post("NEW THING","ITS SUPER COOL","../images/stacks.jpg", "www.google.com", "CLICK THIS TO GO TO WEBSITE", "cool image");
-    place.build();
+  // if the season divs are on the page then use this code, else ignore it
+  try {
+    document.getElementById("springDiv").addEventListener('click', function(){
+      localStorage.setItem("spring", "true");
+    });
+    document.getElementById("summerDiv").addEventListener('click', function() {
+      localStorage.setItem("summer", "true");
+    });
+    document.getElementById("fallDiv").addEventListener('click', function() {
+      localStorage.setItem("fall", "true");
+    });
+    document.getElementById("winterDiv").addEventListener('click', function() {
+      localStorage.setItem("winter", "true");
+    });
+  } catch (e) {
+  } finally {
+  }
+
+  // if the price divs are on the page then use this code, else ignore it
+  try {
+    document.getElementById("cheapDiv").addEventListener('click', function() {
+      localStorage.setItem("cheap", "true");
+    });
+    document.getElementById("fairDiv").addEventListener('click', function() {
+      localStorage.setItem("fair", "true");
+    });
+    document.getElementById("expensiveDiv").addEventListener('click', function() {
+      localStorage.setItem("expensive", "true");
+    });
+  } catch (e) {
+  } finally {
+  }
+
+//set all the season and price values to be false
+  try {
+    document.getElementById("back").addEventListener('click', function() {
+      localStorage.setItem("spring", "false");
+      localStorage.setItem("summer", "false");
+      localStorage.setItem("fall", "false");
+      localStorage.setItem("winter", "false");
+
+      localStorage.setItem("cheap", "false");
+      localStorage.setItem("fair", "false");
+      localStorage.setItem("expensive", "false");
+    });
+  } catch (e) {
+  } finally {
+  }
+  let place = new Post("NEW THING", "ITS SUPER COOL", "../images/stacks.jpg", "www.google.com", "CLICK THIS TO GO TO WEBSITE", "cool image");
 
 
 }
